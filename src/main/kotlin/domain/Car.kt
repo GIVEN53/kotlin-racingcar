2fb79fa42455a9f09d183711863a5fd3aa367461
Gibeom Nam <rlqja0523@naver.com>
2025-06-15 23:53:29 +0200

    refactor: replace companion object with const val

--- src/main/kotlin/domain/Car.kt
+++ src/main/kotlin/domain/Car.kt
@@ -2,6 +2,10 @@ package domain
 
 import numbergenerator.NumberGenerator
 
+private const val MIN_NAME_LENGTH = 1
+private const val MAX_NAME_LENGTH = 5
+private const val MOVE_THRESHOLD = 4
+
 class Car(
     val name: String,
 ) {
@@ -13,12 +17,6 @@ class Car(
         require(
             name.length in MIN_NAME_LENGTH..MAX_NAME_LENGTH,
         ) { "Car name must be between $MIN_NAME_LENGTH and $MAX_NAME_LENGTH characters" }
-    }
-
-    companion object {
-        private const val MIN_NAME_LENGTH = 1
-        private const val MAX_NAME_LENGTH = 5
-        private const val MOVE_THRESHOLD = 4
     }
 
     fun move(numberGenerator: NumberGenerator) {
